ffba85cac9f3dd281ddd616e37137ece43d31581
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-02-09 19:07:43 +0200

    feat: create the sample app

--- sampleapp/src/androidTest/java/io/github/rosariopfernandes/firecoilsampleapp/ExampleInstrumentedTest.kt
+++ sampleapp/src/androidTest/java/io/github/rosariopfernandes/firecoilsampleapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.github.rosariopfernandes.firecoil", appContext.packageName)
+        assertEquals("io.github.rosariopfernandes.firecoilsampleapp", appContext.packageName)
     }
 }
