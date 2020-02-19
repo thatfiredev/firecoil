7a201605867338db636dccf58a99edfacb185ec1
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-02-19 22:38:31 +0200

    feat: create the FireCoil singleton (#9)
    this class can be used to get a default ImageLoader that
     already includes the StorageReferenceFetcher

--- firecoil/src/main/java/io/github/rosariopfernandes/firecoil/ImageViews.kt
+++ firecoil/src/main/java/io/github/rosariopfernandes/firecoil/ImageViews.kt
@@ -1,7 +1,6 @@
 package io.github.rosariopfernandes.firecoil
 
 import android.widget.ImageView
-import coil.ImageLoader
 import coil.api.loadAny
 import coil.request.LoadRequestBuilder
 import coil.request.RequestDisposable
@@ -11,11 +10,7 @@ inline fun ImageView.load(
     data: StorageReference,
     builder: LoadRequestBuilder.() -> Unit = {}
 ): RequestDisposable {
-    val imageLoader = ImageLoader(context) {
-        componentRegistry {
-            add(StorageReferenceFetcher())
-        }
-    }
+    val imageLoader = FireCoil.loader(context)
     return imageLoader.loadAny(context, data) {
         target(this@load)
         builder()
